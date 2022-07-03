99ff3e8176e2f97e66d85e1b1a016b3f5e6374d5
Juan Orozco <juan.orozcozamora@ucr.ac.cr>
2022-07-02 18:08:14 -0600

    added the treatment logic and view (nacho)

--- ExpedienteMedico_Mob/js/Treatment.js
+++ ExpedienteMedico_Mob/js/Treatment.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     for (let i = 0; i < 3; i++) {
-        $("#affections_container").append(getAffectionElement(i));
+        $("#treatment_container").append(getAffectionElement(i));
     }
     //collapse all accordion items
     $('.collapse').collapse('hide');
@@ -12,20 +12,20 @@ const getAffectionElement = (i) => {
 <h2 class="accordion-header" id=heading${i}>
     <button class="accordion-button" type="button" data-bs-toggle="collapse"
         data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
-        Padecimiento #${i}
+       Tratamiento #${i}
     </button>
 </h2>
 <div id="collapse${i}" class="accordion-collapse collapse show" aria-labelledby="heading${i}"
     data-bs-parent="#affections_container">
     <div class="accordion-body">
-        <strong>Detalles del padecimiento.</strong>
+        <strong>Detalles del tratamiento.</strong>
     </div>
-    ${affectionDescription}
+    ${treatmentDescription}
 </div>
 </div>`;
 }
 
-const affectionDescription =
+const treatmentDescription =
     `<div class="container py-2">
         <div class="row">
             <textarea class="py-2"></textarea>
