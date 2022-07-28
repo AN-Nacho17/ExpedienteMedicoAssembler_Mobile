6ba86326fa6ee120e1bdd3df6f1d028c84ff4049
Juan Orozco <juan.orozcozamora@ucr.ac.cr>
2022-07-27 23:53:36 -0600

    added more fron end to the login page

--- ExpedienteMedico_Mob/js/Tiny.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    tinymce.init({
-        selector: "textarea",
-        height: 300,
-        menubar: false,
-        toolbar: false,
-        readonly: true
-    });
-});
