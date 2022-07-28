76b95f97cd3ab5c251cd0fe6c91d66eda30b5afd
Juan Orozco <juan.orozcozamora@ucr.ac.cr>
2022-07-28 03:05:20 -0600

    se agregar la funcionalidad entera de notas medicas la branch alter

--- ExpedienteMedico_Mob/js/ClinicHistory.js
+++ ExpedienteMedico_Mob/js/ClinicHistory.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             success: function (data) {
                 if (data.success) {
                     console.log(data.data);
-                    getNotes(data.data.user);
+                    getNotes(data.data);
                 } else {
                     Swal.fire({
                         title: "Error",
@@ -29,38 +29,37 @@ $(document).ready(function () {
             button: "Aceptar",
         });
     }
-});
 
-function getNotes() {
-    for (let i = 0; i < 3; i++) {
-        $("#medicNotes_list").append(getMedicNote(i));
+    function getNotes(data) {
+        for (let i = 0; i < data.length; i++) {
+            $("#medicNotes_list").append(getMedicNote(data[i]));
+        }
     }
-}
 
-const getMedicNote = (i) => {
-    let item =
-        `  <div class="row">
-    <li class="medicNote list-group-item">
-        <div class="col-md-6 mx-auto">
-            <div class="medicNote_label container text-center">
-                <label>
-                    Fecha de registro: 7/3/2022:10:36 PM
-                </label>
+    const getMedicNote = (data) => {
+        //spanish dat
+        let date = new Date(data.noteDate);
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        let dateString = new Date(date).toLocaleDateString("es-ES", options);
+        let item =
+            `  <div class="row">
+        <li class="medicNote list-group-item">
+            <div class="col-md-6 mx-auto">
+                <div class="medicNote_label container text-center">
+                    <label>
+                        ${dateString}
+                    </label>
+                </div>
             </div>
-        </div>
-        <div class="col-md-6 mx-auto">
-            <div class="container text-center py-3">
-                <button onClick="loadMedicNote()"class="medicNote_button">
-                    Ver nota
-                    <img src="/img/_next.svg" alt="Medicine">
-                </button>
+            <div class="col-md-6 mx-auto">
+                <div id="note_description" class="container text-center py-3">
+                    ${data.note}
+                </div>
             </div>
-        </div>
-    </li>
-</div>`;
-    return item;
-}
+        </li>
+    </div>`;
+        return item;
+    }
 
-function loadMedicNote() {
-    document.location.href = "MedicNote.html";
-}
+
+});
