244e593be13877eac727436ad1a137ec96e645e7
Juan Orozco <juan.orozcozamora@ucr.ac.cr>
2022-07-28 00:21:02 -0600

    added the lab views and js

---
+++ ExpedienteMedico_Mob/js/Lab.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function () {
+
+    let user = JSON.parse(localStorage.getItem("user"));
+    if (user != null) {
+        const id = user.id;
+        const _url = `https://localhost:44346/Api/Api/lab?id=${id}`;
+        $.ajax({
+            type: "GET",
+            url: _url,
+            success: function (data) {
+                if (data.success) {
+                    console.log(data.data);
+                    getLab(data.data)
+                } else {
+                    Swal.fire({
+                        title: "Error",
+                        text: data.data,
+                        icon: "error",
+                        button: "Aceptar",
+                    });
+                }
+            }
+        });
+    } else {
+        Swal.fire({
+            title: "Error",
+            text: "No hay ningun usuario logueado",
+            icon: "error",
+            button: "Aceptar",
+        });
+    }
+
+    function getLab(data) {
+        for (let i = 0; i < data.length; i++) {
+            $("#lab_container").append(getLabElement(data[i], i));
+        }
+    }
+
+    const getLabElement = (data, i) => {
+        return `<div class="accordion-item">
+    <h2 class="accordion-header" id=lab-heading${i}>
+        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+            data-bs-target="#lab-collapse${i}" aria-expanded="false" aria-controls="lab-collapse${i}">
+            ${data.name}
+        </button>
+    </h2>
+    <div id="lab-collapse${i}" class="accordion-collapse collapse show" aria-labelledby="lab-heading${i}"
+        data-bs-parent="#lab_container">
+        <div class="accordion-body">
+            <strong>Detalles del padecimiento.</strong>
+        </div>
+        <div class="container py-2">
+            <div class="row">
+                <div id="accordion-description">
+                    ${data.description}
+                </div>
+            </div >
+        </div >
+    </div >
+    </div > `;
+    }
+
+});
